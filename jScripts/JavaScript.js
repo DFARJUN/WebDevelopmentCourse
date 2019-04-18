d9971bfd5a1ab4dfaa1a7acbe0888bde3577fb3f
DFARJUN <49616584+DFARJUN@users.noreply.github.com>
2019-04-18 18:41:34 +0300

    s

--- jScripts/JavaScript.js
+++ jScripts/JavaScript.js
@@ -1,1 +1,13 @@
-﻿
+﻿//window.onscroll = myFunction4;
+function myFunction4() {
+    var wind = window.innerHeight;
+    var wind2 = window.innerWidth;
+    var yoff = window.pageYOffset;
+    var dordor = document.getElementById("mania").getBoundingClientRect().y;
+    if ((wind / 2) < dordor && wind > dordor) {
+        var fff = dordor - (wind / 2)
+        if (fff < wind2-1200) {
+ document.documentElement.style.setProperty('--mania-x', fff + "px");
+        }
+    }
+}
